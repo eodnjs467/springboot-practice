99abeb2edfb22f81b94d277e16fa15a2a8d0c2f7
DAEWON <sponjjanc@naver.com>
2021-05-10 21:08:56 +0900

    Search Acacdemy Project

---
+++ testdb/src/test/java/com/example/testdb/TestdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
