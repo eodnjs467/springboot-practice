51e6ca466a402995075b44e564aeb1c8b49364fb
DAEWON <sponjjanc@naver.com>
2021-06-02 11:01:52 +0900

    웹 계층 개발(회원 등록)

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -1,10 +1,18 @@
 package jpabook.jpashop.controller;
 
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter @Setter
 public class MemberForm {
 
-    private
+    @NonNull
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+
 }
