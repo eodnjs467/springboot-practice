d1d187cd1d32552bb3f5694961fe3dd69026c273
DAEWON <sponjjanc@naver.com>
2021-06-02 12:10:17 +0900

    상품 등록, 조회

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class BookForm {
+
+    private Long id;    // 상품 수정 때문에 id 값 받아야함
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
